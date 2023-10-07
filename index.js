9d8f18df8b7c445d28f84553c643ef2d43b5fea6
PITTA VENKATA VAMSI MADHAV <110703122+vmcse@users.noreply.github.com>
2023-10-07 21:07:13 +0530

    Created basic layout in html and functions for each operation

---
+++ index.js
@@ -0,0 +1,19 @@
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+const operate = (x, operator, y) => {
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+    default:
+      return "Invalid Operator";
+  }
+};
